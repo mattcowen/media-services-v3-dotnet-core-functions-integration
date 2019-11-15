b43cee3246f159df6e2b07ad4437591bf0e45a8a
Luke Devonshire <luked@microsoft.com>
2019-11-15 12:20:54 +0000

    Reverted changes to Submit MediaJob

--- advanced-vod-workflow/Functions/SubmitMediaJob.cs
+++ advanced-vod-workflow/Functions/SubmitMediaJob.cs
@@ -71,35 +71,21 @@ namespace advanced_vod_functions_v3
             log.LogInformation($"AMS v3 Function - SubmitMediaJob was triggered!");
 
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
             // Validate input objects
             if (data.inputAssetName == null)
-            {
                 return new BadRequestObjectResult("Please pass inputAssetName in the input object");
-            }
             if (data.transformName == null)
-            {
                 return new BadRequestObjectResult("Please pass transformName in the input object");
-            }
             if (data.outputAssetNamePrefix == null)
-            {
                 return new BadRequestObjectResult("Please pass outputAssetNamePrefix in the input object");
-            }
-
             string inputAssetName = data.inputAssetName;
-
             string transformName = data.transformName;
-
             string outputAssetNamePrefix = data.outputAssetNamePrefix;
-
             string assetStorageAccount = null;
-
             if (data.assetStorageAccount != null)
-            {
                 assetStorageAccount = data.assetStorageAccount;
-            }
 
             MediaServicesConfigWrapper amsconfig = new MediaServicesConfigWrapper();
             Asset inputAsset = null;
@@ -116,77 +102,45 @@ namespace advanced_vod_functions_v3
                 IAzureMediaServicesClient client = MediaServicesHelper.CreateMediaServicesClientAsync(amsconfig);
 
                 inputAsset = client.Assets.Get(amsconfig.ResourceGroup, amsconfig.AccountName, inputAssetName);
-
                 if (inputAsset == null)
-                {
                     return new BadRequestObjectResult("Asset for input not found");
-                }
-
                 Transform transform = client.Transforms.Get(amsconfig.ResourceGroup, amsconfig.AccountName, transformName);
-
                 if (transform == null)
-                {
                     return new BadRequestObjectResult("Transform not found");
-                }
 
                 var jobOutputList = new List<JobOutput>();
-
                 for (int i = 0; i < transform.Outputs.Count; i++)
                 {
                     Guid assetGuid = Guid.NewGuid();
-
                     string outputAssetName = outputAssetNamePrefix + "-" + assetGuid.ToString();
-
                     Asset assetParams = new Asset(null, outputAssetName, null, assetGuid, DateTime.Now, DateTime.Now, null, outputAssetName, null, assetStorageAccount, AssetStorageEncryptionFormat.None);
-
                     Asset outputAsset = client.Assets.CreateOrUpdate(amsconfig.ResourceGroup, amsconfig.AccountName, outputAssetName, assetParams);
-
                     jobOutputList.Add(new JobOutputAsset(outputAssetName));
 
                     Preset preset = transform.Outputs[i].Preset;
                     if (encoderOutputAssetName == null && (preset is BuiltInStandardEncoderPreset || preset is StandardEncoderPreset))
-                    {
                         encoderOutputAssetName = outputAssetName;
-                    }
-
                     if (audioAnalyzerOutputAssetName == null && preset is AudioAnalyzerPreset)
-                    {
                         audioAnalyzerOutputAssetName = outputAssetName;
-                    }
-
                     if (videoAnalyzerOutputAssetName == null && preset is VideoAnalyzerPreset)
-                    {
                         videoAnalyzerOutputAssetName = outputAssetName;
-                    }
 
                     // set up jobOutputs
                     JObject outObject = new JObject();
-
                     outObject["Preset"] = preset.ToString().Split('.').Last<string>();
-
                     outObject["OutputAssetName"] = outputAssetName;
-
                     if (preset is BuiltInStandardEncoderPreset || preset is StandardEncoderPreset)
-                    {
                         outObject["Category"] = "Encoder";
-                    }
                     else if (preset is AudioAnalyzerPreset || preset is VideoAnalyzerPreset)
-                    {
                         outObject["Category"] = "Analyzer";
-                    }
                     else
-                    {
                         outObject["Category"] = "Unknown";
-                    }
-
                     outputs.Add(outObject);
                 }
 
                 // Use the name of the created input asset to create the job input.
                 JobInput jobInput = new JobInputAsset(assetName: inputAssetName);
-
                 JobOutput[] jobOutputs = jobOutputList.ToArray();
-
                 Job job = client.Jobs.Create(
                     amsconfig.ResourceGroup,
                     amsconfig.AccountName,
